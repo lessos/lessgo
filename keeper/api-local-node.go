1acb5543da0015d0add0af33d7ce8f98ba44c456
Eryx <evorui@gmail.com>
2013-07-30 00:02:56 +0800

    [NEW] API LocalNodeListAndGet() for lessgo

--- keeper/api-local-node.go
+++ keeper/api-local-node.go
@@ -16,6 +16,40 @@ func (this *Keeper) LocalNodeList(path string) (rpl *Reply) {
     return this.req("POST", req)
 }
 
+func (this *Keeper) LocalNodeListAndGet(path string) (rpl *Reply) {
+
+    rpl = this.LocalNodeList(path)
+
+    str, err := rpl.Str()
+    if err != nil {
+        rpl.Type = ReplyError
+        return
+    }
+
+    var lsis []Node
+    if err := JsonDecode(str, &lsis); err != nil {
+        rpl.Type = ReplyError
+        return
+    }
+
+    rpl.Type = ReplyMulti
+    for _, v := range lsis {
+
+        if v.T != NodeTypeFile {
+            continue
+        }
+
+        rs := this.LocalNodeGet(path + "/" + v.P)
+        if rs.Type == ReplyError {
+            continue
+        }
+
+        rpl.Elems = append(rpl.Elems, rs)
+    }
+
+    return
+}
+
 func (this *Keeper) LocalNodeSet(path, val string) (rpl *Reply) {
     req := map[string]string{
         "method": "locset",
