6182ceda0379c302de3cf42caffc765171411026
Eryx <evorui@gmail.com>
2013-10-09 23:29:54 +0800

    [NEW] 新增接口,获取系统可用端口

---
+++ utils/net.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+    "math/rand"
+    "net"
+    "strconv"
+    "time"
+    "errors"
+)
+
+
+func NetFreePort(start, end int) (error, int, string) {
+
+    if end > 65535 {
+        end = 65535
+    }
+    if start < 1 {
+        start = 1
+    }
+    if (end - start) < 100 {
+        start = 1
+        end = 65535
+    }
+
+    try := 100
+    r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+    for {
+
+        if try < 0 {
+            return errors.New("Error"), 0, "0"
+        }
+        iport := start + r.Intn(end - start)
+
+        port := strconv.Itoa(iport)
+        ln, err := net.Listen("tcp", ":"+port)
+        if err == nil {
+            ln.Close()
+            return nil, iport, port
+        }
+
+        try--
+    }
+}
