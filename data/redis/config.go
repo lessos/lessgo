d943df9c506d939ab5217a33a550c5a4d68c72e1
Eryx <evorui@gmail.com>
2014-07-07 21:43:39 +0800

    [NEW] new redis-protocol compatible client

---
+++ data/redis/config.go
@@ -0,0 +1,22 @@
+package redis
+
+type Config struct {
+
+    // Database server hostname or IP. Leave blank if using unix sockets
+    Host string `json:"host"`
+
+    // Database server port. Leave blank if using unix sockets
+    Port string `json:"port"`
+
+    // Password for authentication
+    Pass string `json:"pass"`
+
+    // A path of a UNIX socket file. Leave blank if using host and port
+    Socket string `json:"socket"`
+
+    // The connection timeout to a redis host (seconds)
+    Timeout int `json:"timeout"`
+
+    // Maximum number of connections
+    MaxConn int `json:"maxconn"`
+}
