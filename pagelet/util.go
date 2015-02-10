0ebc13a4e9c939091024179bbbe3bba12ab1c414
Eryx <evorui@gmail.com>
2015-02-10 22:09:39 +0800

    pagelet/vutils/pager.go Code Refact

--- pagelet/util.go
+++ pagelet/util.go
@@ -1,7 +1,6 @@
 package pagelet
 
 import (
-	"math"
 	"reflect"
 )
 
@@ -87,96 +86,4 @@ func GreaterThan(a, b interface{}) bool {
 // GreaterEqual evaluates the comparison a >= b
 func GreaterEqual(a, b interface{}) bool {
 	return !LessThan(a, b)
-}
-
-type Paginator struct {
-	ItemCount         int
-	CountPerPage      int
-	PageCount         int
-	CurrentPageNumber int
-	FirstPageNumber   int
-	PrevPageNumber    int
-	NextPageNumber    int
-	LastPageNumber    int
-	RangeLen          int
-	RangeStartNumber  int
-	RangeEndNumber    int
-	RangePages        []int
-}
-
-// Pager build a page collection
-//
-// @param int current  Number of Current page
-// @param int count    Total number of items available
-// @param int perpage  Number of items per page
-// @param int rangelen Number of discrete page numbers that will be displayed
-func Pager(current, count, perpage, rangelen int) Paginator {
-
-	pager := Paginator{
-		ItemCount:         count,
-		CountPerPage:      perpage,
-		CurrentPageNumber: current,
-		RangeLen:          rangelen,
-	}
-
-	//
-	if pager.ItemCount < 0 {
-		pager.ItemCount = 0
-	}
-
-	if pager.CountPerPage < 1 {
-		pager.CountPerPage = 10
-	}
-
-	if pager.RangeLen < 1 {
-		pager.RangeLen = 1
-	}
-
-	pager.PageCount = pager.ItemCount / pager.CountPerPage
-	if v := math.Mod(float64(pager.ItemCount), float64(pager.CountPerPage)); v > 0 {
-		pager.PageCount++
-	}
-
-	// fixed current page number
-	if pager.CurrentPageNumber < 1 {
-		pager.CurrentPageNumber = 1
-	} else if pager.CurrentPageNumber > pager.PageCount {
-		pager.CurrentPageNumber = pager.PageCount
-	}
-
-	//
-	pager.RangeStartNumber = 1
-	if (pager.CurrentPageNumber - rangelen/2) > 0 {
-		pager.RangeStartNumber = pager.CurrentPageNumber - rangelen/2
-	}
-
-	pager.RangeEndNumber = pager.PageCount
-	if (pager.RangeStartNumber + rangelen) < pager.PageCount {
-		pager.RangeEndNumber = pager.RangeStartNumber + rangelen - 1
-	}
-
-	// taking previous page
-	if pager.CurrentPageNumber > 1 {
-		pager.PrevPageNumber = pager.CurrentPageNumber - 1
-	}
-
-	// taking next page
-	if pager.CurrentPageNumber < pager.PageCount {
-		pager.NextPageNumber = pager.CurrentPageNumber + 1
-	}
-
-	// taking pages list
-	for i := pager.RangeStartNumber; i <= pager.RangeEndNumber; i++ {
-		pager.RangePages = append(pager.RangePages, i)
-	}
-
-	if pager.RangeStartNumber > 1 {
-		pager.FirstPageNumber = 1
-	}
-
-	if pager.RangeEndNumber < pager.PageCount {
-		pager.LastPageNumber = pager.PageCount
-	}
-
-	return pager
 }
