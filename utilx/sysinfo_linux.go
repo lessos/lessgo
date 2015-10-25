2bf8052e0b051c51e947b09df7ce7fe6c5c69b8b
Eryx <evorui@gmail.com>
2015-10-25 16:21:18 +0800

    code refact for sysinfo

--- utilx/sysinfo_linux.go
+++ utilx/sysinfo_linux.go
@@ -7,20 +7,6 @@ import (
 	"time"
 )
 
-type Sysinfo struct {
-	CpuNum    int       `json:"cn"`
-	Uptime    int64     `json:"ut"`
-	Loads     [3]uint64 `json:"ld"`
-	MemTotal  uint64    `json:"mt"`
-	MemFree   uint64    `json:"mf"`
-	MemShared uint64    `json:"ms"`
-	MemBuffer uint64    `json:"mb"`
-	SwapTotal uint64    `json:"st"`
-	SwapFree  uint64    `json:"sf"`
-	Procs     uint16    `json:"pc"`
-	TimeNow   string    `json:"tn"`
-}
-
 func SysinfoGet() Sysinfo {
 
 	var si syscall.Sysinfo_t
